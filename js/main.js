db28ebb6bb781edf9b4f7b023ebb62eeca10f7a5
Aurélie CANONNE <aurelie.canonne@outlook.fr>
2022-06-09 09:51:13 +0200

    Initialization of distributeurjs project

---
+++ js/main.js
@@ -0,0 +1,24 @@
+// Initialization of an array containing all drinks
+const drinks = [
+    {
+        id: 1,
+        name: "Coca Cola",
+        price: 0.8
+    },
+    {
+        id: 2,
+        name: "Fanta",
+        price: 0.7
+    },
+    {
+        id: 3,
+        name: "Ice Tea",
+        price: 0.7
+    },
+    {
+        id: 4,
+        name: "Perrier",
+        price: 1
+    }
+];
+
